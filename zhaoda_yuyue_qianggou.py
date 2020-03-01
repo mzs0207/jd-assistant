a6da2fc5afa09655844a84fc19974db25448cae2
mengzengshan <378077287@qq.com>
2020-03-01 15:14:25 +0800

    初始化能用6

--- zhaoda_yuyue_qianggou.py
+++ zhaoda_yuyue_qianggou.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         sku_ids = '65624145328'  # 商品id
         area = '19_1607_3639'  # 区域id
         yue_yue_time = '{0} 11:00:00.5'.format(today.strftime("%Y-%m-%d"))  # 预约时间
-        buy_time = '{0} 15:00:00.5'.format(today.strftime("%Y-%m-%d"))
+        buy_time = '{0} 15:00:00.2'.format(today.strftime("%Y-%m-%d"))
         asst = Assistant()  # 初始化
         asst.login_by_QRcode()  # 扫码登陆
         asst.make_reserve_by_time(sku_ids, yue_yue_time)  # 执行预约
